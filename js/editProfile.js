3a15f93c09b49c382f28c7d57c73bbb398958575
rom-L <larrondoromeo@gmail.com>
2023-07-01 19:05:47 -0300

    avance edit profile

--- js/editProfile.js
+++ js/editProfile.js
@@ -1,23 +1,41 @@
 "use strict";
 
-let editButtons = document.querySelectorAll('edit-button');
+let editButtons = document.querySelectorAll('.edit-button');
 let editPopup = document.querySelector('.edit-popup-container');
-let formEditPopup = document.querySelector('#form-edit-popup');
+let email = document.querySelector('#profile-email');
+let address = document.querySelector('#profile-address');
+let editError = document.querySelector('#edit-error');
 let acceptButton = document.querySelector('#edit-popup-btn-accept');
 let declineButton = document.querySelector('#edit-popup-btn-decline');
 
 for (const BUTTON of editButtons) {
     BUTTON.addEventListener("click", function(e) {
+        e.preventDefault();
         editPopup.classList.remove("hidden");
+        
+        let atributo = BUTTON.getAttribute("data-id");
+        let field = document.querySelector('#edit-field');
+        console.log(atributo)
+
+        acceptButton.addEventListener("click", function(e) {
+            if (field.value !== '') {
+                if (atributo == 'email') {
+                    email.textContent = field.value;
+                    editPopup.classList.add("hidden");
+                    editError.classList.add("hidden");
+                } else if (atributo == 'address') {
+                    address.textContent = field.value;
+                    editPopup.classList.add("hidden");
+                    editError.classList.add("hidden");
+                }
+            } else {
+                editError.classList.remove("hidden");
+            }
+            
+        });
+
+        declineButton.addEventListener("click", function(e) {
+            editPopup.classList.add("hidden");
+        });
     });
 }
-
-// formEditPopup.addEventListener("submit", function(e) {
-//     e.preventDefault();
-//     let formData = new FormData(formEditPopup);
-//     let field = formData.get("edit-field");
-
-//     //hacer algo con el valor del field
-
-//     editPopup.classList.add("hidden");
-// });
